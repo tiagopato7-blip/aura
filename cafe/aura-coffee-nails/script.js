c4ca19be58ff084f52031829c8a3f9b985891a76
tiago pato <tiagopato7@gmail.com>
2026-05-29 21:49:30 +0200

    feat(hero): remove horizontal ticker tape marquee banner and position header perfectly at top

--- cafe/aura-coffee-nails/script.js
+++ cafe/aura-coffee-nails/script.js
@@ -394,7 +394,7 @@ if (contactBtn && contactModal && closeContactBtn) {
         } else {
             header.style.transform = 'translateY(0)';
             header.style.opacity = '1';
-            header.style.top = '60px';
+            header.style.top = '20px';
         }
         lastScrollY = scrollY;
     };
